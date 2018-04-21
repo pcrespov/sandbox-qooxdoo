880be2cbcec891016afbe6fa60516ae61baf179e
Pedro Crespo-Valero <crespo@itis.ethz.ch>
2018-04-21 15:53:35 +0200

    Connected preferences to user button. Added other minor changes

--- basic-samples/wam/source/class/wam/ui/ContentPage.js
+++ basic-samples/wam/source/class/wam/ui/ContentPage.js
@@ -4,63 +4,81 @@ qx.Class.define("wam.ui.ContentPage", {
     construct: function () {
         this.base(arguments, new qx.ui.layout.VBox(0));
 
+        this.buildLayout();
 
-        let toolbar = new qx.ui.container.Composite(new qx.ui.layout.HBox()); {
-            toolbar.set({
+        // Connect actions
+        this.__userBtn.addListener("execute", function(){
+            if (!this.__preferencesWin)
+            {
+                this.__preferencesWin = new wam.ui.Preferences();
+                this.__preferencesWin.moveTo(0,0); // TODO: center
+            }
+            this.__preferencesWin.open();
+        }, this)
+
+    },
+
+    members: {
+        __preferencesWin: null, 
+        __userBtn: null,        
+
+        buildLayout: function()
+        {
+            let topBar = new qx.ui.container.Composite(new qx.ui.layout.HBox());
+            this.buildTopLayout(topBar);
+            this.add(topBar);
+            
+            // Project content: New project, templates, and recent ...
+            this.add(new qx.ui.core.Widget().set({
+                backgroundColor: "green"
+            }), { flex: 1 });
+        },
+
+        buildTopLayout: function(topBar)
+        {
+            topBar.set({
                 backgroundColor: "orange",
                 height: 70,
                 paddingLeft: 10,
                 paddingRight: 10,
             });
-            toolbar.getLayout().set({
+            topBar.getLayout().set({
                 spacing: 10,
                 alignY: "middle",
             });
 
-            // $(width)x$(height)/$(background)/$(fontcolor)/
-            const urlPrefix = "https://imgplaceholder.com/32x32/transparent/757575/";
+            // $(width)x$(height)/$(background)/$(fontcolor)/            
+            const iconUrl = wam.utils.placeholders.getIcon("fa-align-justify", 32);
             const btnSettings = {
                 allowGrowY: false,
                 minWidth: 32,
                 minHeight: 32,
             }
-            
-            let menuBtn = new qx.ui.form.Button(null, urlPrefix + "fa-align-justify")
-            menuBtn.set(btnSettings);
-            toolbar.add(menuBtn);
+
+            // let menuBtn = new qx.ui.form.Button(null, iconUrl)
+            // menuBtn.set(btnSettings);
+            // topBar.add(menuBtn);
 
             let titleAtm = new qx.ui.basic.Atom(" simcore logo ");
-            toolbar.add(titleAtm);
-
+            topBar.add(titleAtm);
 
             let searchTxt = new qx.ui.form.TextField().set({
                 placeholder: "Search"
             });
 
-            toolbar.add(searchTxt, {
+            topBar.add(searchTxt, {
+                flex: 2
+            });
+
+            topBar.add(new qx.ui.core.Spacer(1), {
                 flex: 1
             });
 
-            toolbar.add(new qx.ui.core.Spacer(1), {
-                flex: 2
-            });
-            
-            let userBtn = new qx.ui.form.Button(null, urlPrefix + "glyphicon-user");
+            let userBtn = this.__userBtn = new qx.ui.form.Button(null, iconUrl);
             userBtn.set(btnSettings);
-            toolbar.add(userBtn);
+            topBar.add(userBtn);
         }
-
-        this.add(toolbar);
-
-        this.add(new qx.ui.core.Widget().set({
-            backgroundColor: "green"
-        }), {
-            flex: 1
-        });
-    },
-
-    members: {
-
-    }
+        
+    } // members
 
 });
