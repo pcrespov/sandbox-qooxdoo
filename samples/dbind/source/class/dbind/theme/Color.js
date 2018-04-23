c32651a42d4981f46dacb08bc8a02e2d84a78294
Pedro Crespo-Valero <crespo@itis.ethz.ch>
2018-04-23 15:32:37 +0200

    Started application to test databinding features

---
+++ samples/dbind/source/class/dbind/theme/Color.js
@@ -0,0 +1,18 @@
+/* ************************************************************************
+
+   Copyright: 2018 undefined
+
+   License: MIT license
+
+   Authors: undefined
+
+************************************************************************ */
+
+qx.Theme.define("dbind.theme.Color",
+{
+  extend : qx.theme.indigo.Color,
+
+  colors :
+  {
+  }
+});
---
+++ samples/dbind/source/class/dbind/theme/Theme.js
@@ -0,0 +1,21 @@
+/* ************************************************************************
+
+   Copyright: 2018 undefined
+
+   License: MIT license
+
+   Authors: undefined
+
+************************************************************************ */
+
+qx.Theme.define("dbind.theme.Theme",
+{
+  meta :
+  {
+    color : dbind.theme.Color,
+    decoration : dbind.theme.Decoration,
+    font : dbind.theme.Font,
+    icon : qx.theme.icon.Tango,
+    appearance : dbind.theme.Appearance
+  }
+});
