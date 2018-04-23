9978edffcccf72c6f7b1e5d86ff31a46932c5d63
Pedro Crespo-Valero <crespo@itis.ethz.ch>
2018-04-23 18:48:48 +0200

    Separated application's layout, data and binding

--- samples/dbind/source/class/dbind/Application.js
+++ samples/dbind/source/class/dbind/Application.js
@@ -3,9 +3,8 @@
  *
  * @asset(dbind/*)
  */
-qx.Class.define("dbind.Application",
-{
-  extend : qx.application.Standalone,
+qx.Class.define("dbind.Application", {
+  extend: qx.application.Standalone,
 
   /*
   *****************************************************************************
@@ -13,87 +12,36 @@ qx.Class.define("dbind.Application",
   *****************************************************************************
   */
 
-  members :
-  {
+  members: {
     /**
      * This method contains the initial application code and gets called 
      * during startup of the application
      * 
      * @lint ignoreDeprecated(alert)
      */
-    main : function()
-    {
+    main: function () {
       // Call super class
       this.base(arguments);
 
       // Enable logging in debug variant
-      if (qx.core.Environment.get("qx.debug"))
-      {
+      if (qx.core.Environment.get("qx.debug")) {
         // support native logging capabilities, e.g. Firebug for Firefox
         qx.log.appender.Native;
         // support additional cross-browser console. Press F7 to toggle visibility
         qx.log.appender.Console;
       }
 
-      /*
-      -------------------------------------------------------------------------
-        Below is your actual application code...
-      -------------------------------------------------------------------------
-      */
+      this.__buildLayout();
 
-      let rawData = [];
-      rawData.push(qx.data.marshal.Json.createModel({
-        name: "New Project",
-        description: "Empty",
-        thumbnail: "https://imgplaceholder.com/171x96/cccccc/757575/ion-plus-round",
-        created: null
-      }));
 
-      for (var i = 0; i < 5; i++) {
-        var item = qx.data.marshal.Json.createModel({
-          name: "Project #" + (i + 1),
-          description: "This is a very short description",
-          thumbnail: null,
-          created: null
-        });
-        rawData.push(item);
-      }
-
+      // Data model --------------
+      const rawData = dbind.data.Fake.createProjectDescriptors();
       // http://www.qooxdoo.org/current/api/#qx.data.Array
       // A wrapper around raw array to make it "bindable"
       var data = new qx.data.Array(rawData);
 
-
-      // Layout -------------
-      let layout = new qx.ui.layout.VBox().set({
-        spacing: 10
-      })
-      let container = new qx.ui.container.Composite(layout).set({
-        backgroundColor: 'yellow',
-      });
-
-      let searchTxt = new qx.ui.form.TextField().set({
-        placeholder: "Search",
-        liveUpdate: true
-      });
-      container.add(searchTxt);
-
-
-      let list = new qx.ui.form.List();
-      list.set({
-        orientation: "horizontal",
-        spacing: 10,
-        // layout
-        allowGrowY: false
-      });
-      container.add(list);
-
-      this.getRoot().add(container, { edge: 5 });
-
-
-
       // binding ----------------
-      let controller = new qx.data.controller.List(data, list, 'name');
+      let controller = new qx.data.controller.List(data, this.__projectsList, 'name');
 
       // https://www.qooxdoo.org/current/pages/data_binding/single_value_binding.html#options-conversion-and-validation
       // http://www.qooxdoo.org/current/api/#qx.data.SingleValueBinding~bind
@@ -115,25 +63,67 @@ qx.Class.define("dbind.Application",
             }
           }, item, id);
           controler.bindProperty("thumbnail", "icon", {
-            converter: function (data){
-              return data === null? "http://via.placeholder.com/171x96": data;
+            converter: function (data) {
+              return data === null ? "http://via.placeholder.com/171x96" : data;
             }
           }, item, id);
         }
       });
 
-      controller.getSelection().addListener("change", function(e){ 
+      controller.getSelection().addListener("change", function (e) {
         const selectedItem = e.getTarget().toArray()[0];
         console.debug("Selected Item:", selectedItem.getName());
       }, this);
 
 
-      //searchTxt.bind("changeValue", );
-      searchTxt.addListener("changeValue", function(e){
-        console.debug("Search string: ", e.getData());
-        
-      }, this);
+      // creates a form out of
+
+      let controllerFrm = new qx.data.controller.Form(controller.getSelection());
       
+
+      //searchTxt.bind("changeValue", );
+      this.__searchTxf.addListener("changeValue", function (e) {
+        console.debug("Search string: ", e.getData());
+
+      }, this);
+    },
+
+    /**
+     * Components 
+     */    
+    __searchTxf: null,
+
+    __projectsList: null,
+
+    __buildLayout: function () {
+      let layout = new qx.ui.layout.VBox().set({
+        spacing: 10
+      });
+      let container = new qx.ui.container.Composite(layout).set({
+        backgroundColor: 'yellow',
+      });
+
+      let searchTxt = new qx.ui.form.TextField().set({
+        placeholder: "Search",
+        liveUpdate: true
+      });
+      container.add(this.__searchTxf = searchTxt);
+
+
+      let list = new qx.ui.form.List();
+      list.set({
+        orientation: "horizontal",
+        spacing: 0,
+        // layout
+        allowGrowY: false
+      });
+      container.add(this.__projectsList = list);
+
+      this.getRoot().add(container, {
+        edge: 5
+      });
     }
-  }
+
+    
+  } // members
 });
---
+++ samples/dbind/source/class/dbind/data/Fake.js
@@ -0,0 +1,30 @@
+qx.Class.define("dbind.data.Fake", {
+  type: 'static',
+
+  statics: {
+
+    createProjectDescriptors: function (count = 3) {
+      let rawData = [];
+      rawData.push(qx.data.marshal.Json.createModel({
+        name: "New Project",
+        description: "Empty",
+        thumbnail: "https://imgplaceholder.com/171x96/cccccc/757575/ion-plus-round",
+        created: null
+      }));
+
+      for (var i = 0; i < 5; i++) {
+        var item = qx.data.marshal.Json.createModel({
+          name: "Project #" + (i + 1),
+          description: "This is a very short description",
+          thumbnail: null,
+          created: null
+        });
+        rawData.push(item);
+      }
+
+      return rawData;
+    }
+
+  } // statics
+
+});
