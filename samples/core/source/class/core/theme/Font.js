f24c468bcf322620af093759d681e33774a6e13e
Pedro Crespo-Valero <crespo@itis.ethz.ch>
2018-04-26 15:07:34 +0200

    Added project to check core functionality

---
+++ samples/core/source/class/core/theme/Font.js
@@ -0,0 +1,18 @@
+/* ************************************************************************
+
+   Copyright: 2018 undefined
+
+   License: MIT license
+
+   Authors: undefined
+
+************************************************************************ */
+
+qx.Theme.define("core.theme.Font",
+{
+  extend : qx.theme.indigo.Font,
+
+  fonts :
+  {
+  }
+});
