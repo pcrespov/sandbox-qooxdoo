2edba34b30a0f8257cdce7751cda1bef46da37e3
Pedro Crespo-Valero <crespo@itis.ethz.ch>
2018-04-26 08:06:35 +0200

    Added layout for multiple login choices

--- samples/auth/source/class/auth/Application.js
+++ samples/auth/source/class/auth/Application.js
@@ -49,21 +49,47 @@ qx.Class.define("auth.Application", {
       }
 
       // standard login. i.e. using app database
-      let login = new auth.ui.login.Standard();
+      let loginPlatform = new auth.ui.login.Standard();
       
-      // TODO: login could offer different types. eg. standard, NIH, lDAP ...
+      // login could offer different types. eg. standard, NIH, lDAP ...
       // or other third parties. Each login can be added as a different
       // widget. Can be e.g. implemented as a Tabview as in gitlab or 
       // with buttons on the side as in wix
+      let loginExternal = this.__createExternalLogin();
+      
+      let login = new qx.ui.container.Composite(new qx.ui.layout.HBox(30));
+      login.add(loginPlatform, {width:"60%"});
+      login.add(loginExternal);
+
+      //login.setBackgroundColor("blue");
 
       // root is configured as a Canvas here
       this.getRoot().add(login, {
-        left: "20%",
-        top: "20%",
-        width: "60%",
-        height: "40%"
+        left: "10%",
+        top: "10%",
+        height: "30%",
       });
+    },
 
+    __createExternalLogin: function(){
+
+      let layout = new qx.ui.layout.VBox(10).set({
+        alignY: "middle"
+      })
+      let loginExternal = new qx.ui.container.Composite(layout);
+
+      let loginOpenId = new qx.ui.form.Button().set({
+        label: "Continue with openID",
+        //icon: "https://upload.wikimedia.org/wikipedia/commons/8/88/Openid.svg",
+      });
+      loginExternal.add(loginOpenId);
+
+      let loginNIH = new qx.ui.form.Button().set({
+        label: "Continue with NIH",
+        //icon: "auth/nih-419.png",
+      });
+      loginExternal.add(loginNIH);
+      return loginExternal;
     }
   }
 });
