bae8ab84da5fc9f6a0ff7e6bf6f2dd8f3cf3238c
Pedro Crespo-Valero <crespo@itis.ethz.ch>
2018-04-26 10:46:58 +0200

    Added window to external login and reorganized demo

--- samples/auth/source/class/auth/Application.js
+++ samples/auth/source/class/auth/Application.js
@@ -48,55 +48,90 @@ qx.Class.define("auth.Application", {
         //}
       }
 
+      let root = this.getRoot();
+      this.__demo(root);
+
+    },
+
+    __demo: function (root) {
+      /**
+       *  Demo #1
+       * 
+       */
+      // root is configured as a Canvas here
+      root.set({
+        backgroundColor: "#707070",
+      });
+
       // FIXME: focus order when pressing tab
+      let loginGroup = new qx.ui.container.Composite(new qx.ui.layout.HBox(30));
 
       // standard login. i.e. using app database
-      let loginPlatform = new auth.ui.login.Standard();
-      
+      let platformLogin = new auth.ui.login.Standard();
+      loginGroup.add(platformLogin, {
+        width: "60%"
+      });
+
       // login could offer different types. eg. standard, NIH, lDAP ...
       // or other third parties. Each login can be added as a different
       // widget. Can be e.g. implemented as a Tabview as in gitlab or 
       // with buttons on the side as in wix
-      let loginExternal = this.__createExternalLogin();
-      
-      let login = new qx.ui.container.Composite(new qx.ui.layout.HBox(30));
-      login.add(loginPlatform, {width:"60%"});
-      login.add(loginExternal);
+      let externalLogin = this.__createExternalLogin();
+      loginGroup.add(externalLogin);
 
-      //login.setBackgroundColor("blue");
-
-      // root is configured as a Canvas here
-      this.getRoot().set({
-        backgroundColor: "#707070",
-      });
-      this.getRoot().add(login, {
+      root.add(loginGroup, {
         left: "10%",
         top: "10%",
         height: "30%",
       });
     },
 
-    __createExternalLogin: function(){
-
+    __createExternalLogin: function () {
+      /**
+       * For demo purposes
+      */
       let layout = new qx.ui.layout.VBox(10).set({
         alignY: "middle"
       })
-      let loginExternal = new qx.ui.container.Composite(layout);
+      let loginGroup = new qx.ui.container.Composite(layout);
 
       let loginOpenId = new qx.ui.form.Button().set({
         label: "Continue with openID",
         // FIXME: icon size
         //icon: "https://upload.wikimedia.org/wikipedia/commons/8/88/Openid.svg",
       });
-      loginExternal.add(loginOpenId);
+      loginGroup.add(loginOpenId);
 
       let loginNIH = new qx.ui.form.Button().set({
         label: "Continue with NIH",
         // FIXME: icon size
         //icon: "auth/nih-419.png",
       });
-      loginExternal.add(loginNIH);
-      return loginExternal;
-    }
+      loginGroup.add(loginNIH);
+
+      // Connect dummy      
+      loginOpenId.addListener("execute", function (){
+        const img = "https://upload.wikimedia.org/wikipedia/commons/8/88/Openid.svg";
+        
+        let win = new qx.ui.window.Window("External Login");
+        win.setLayout(new qx.ui.layout.Basic());
+        win.setModal(true);
+        win.add(new qx.ui.basic.Image(img));
+        win.open();
+      });
+
+      loginNIH.addListener("execute", function (){
+        const img = "auth/nih-419.png";
+
+        let win = new qx.ui.window.Window("External Login");
+        win.setLayout(new qx.ui.layout.Basic());
+        win.setModal(true);
+        win.add(new qx.ui.basic.Image(img));
+        win.open();
+      });
+
+      return loginGroup;
+    },
+
   }
 });
