cecc55d1fcdf6188660386181edca36106f84dc3
Pedro Crespo-Valero <pedro.crespovalero@gmail.com>
2018-05-24 13:02:12 +0200

    Started new project to explore client-sdie REST

---
+++ samples/rest/source/class/rest/theme/Appearance.js
@@ -0,0 +1,18 @@
+/* ************************************************************************
+
+   Copyright: 2018 
+
+   License: MIT license
+
+   Authors: 
+
+************************************************************************ */
+
+qx.Theme.define("rest.theme.Appearance",
+{
+  extend : qx.theme.indigo.Appearance,
+
+  appearances :
+  {
+  }
+});
---
+++ samples/rest/source/class/rest/theme/Theme.js
@@ -0,0 +1,21 @@
+/* ************************************************************************
+
+   Copyright: 2018 
+
+   License: MIT license
+
+   Authors: 
+
+************************************************************************ */
+
+qx.Theme.define("rest.theme.Theme",
+{
+  meta :
+  {
+    color : rest.theme.Color,
+    decoration : rest.theme.Decoration,
+    font : rest.theme.Font,
+    icon : qx.theme.icon.Tango,
+    appearance : rest.theme.Appearance
+  }
+});
