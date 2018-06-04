3a0fb508aad44904a45d3768c5eae7529dbcf897
Pedro Crespo-Valero <crespo@itis.ethz.ch>
2018-06-04 20:51:45 +0200

    Adde check script

--- dockerfiles/qx-devel/scripts/test.sh
+++ dockerfiles/qx-devel/scripts/test.sh
@@ -1,13 +1,47 @@
 #!/bin/bash
 
 
-if test "$0" = "qx"
-then
+#!/bin/bash
 
-else
-  python generator.py
-
-for FILE1 in "$@" 
+POSITIONAL=()
+while [[ $# -gt 0 ]]
 do
-wc $FILE1
+key="$1"
+
+case $key in
+    -e|--extension)
+    EXTENSION="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -s|--searchpath)
+    SEARCHPATH="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -l|--lib)
+    LIBPATH="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    --default)
+    DEFAULT=YES
+    shift # past argument
+    ;;
+    *)    # unknown option
+    POSITIONAL+=("$1") # save it in an array for later
+    shift # past argument
+    ;;
+esac
 done
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
+echo FILE EXTENSION  = "${EXTENSION}"
+echo SEARCH PATH     = "${SEARCHPATH}"
+echo LIBRARY PATH    = "${LIBPATH}"
+echo DEFAULT         = "${DEFAULT}"
+echo "Number files in SEARCH PATH with EXTENSION:" $(ls -1 "${SEARCHPATH}"/*."${EXTENSION}" | wc -l)
+if [[ -n $1 ]]; then
+    echo "Last line of file specified as non-opt/last argument:"
+    tail -1 "$1"
+fi
