68a86daf95117f63507bb4f5f78d128eac7b277c
Pedro Crespo-Valero <pedro.crespovalero@gmail.com>
2018-06-04 17:21:06 +0200

    WIP: implementing rest api

--- samples/rest/source/class/rest/Application.js
+++ samples/rest/source/class/rest/Application.js
@@ -14,59 +14,76 @@
  * @asset(rest/*)
  */
 qx.Class.define("rest.Application",
-{
-  extend : qx.application.Standalone,
-
-
-
-  /*
-  *****************************************************************************
-     MEMBERS
-  *****************************************************************************
-  */
-
-  members :
   {
-    /**
-     * This method contains the initial application code and gets called 
-     * during startup of the application
-     * 
-     * @lint ignoreDeprecated(alert)
-     */
-    main : function()
-    {
-      // Call super class
-      this.base(arguments);
-
-      // Enable logging in debug variant
-      if (qx.core.Environment.get("qx.debug"))
+    extend: qx.application.Standalone,
+    /*
+    *****************************************************************************
+       MEMBERS
+    *****************************************************************************
+    */
+    members:
       {
-        // support native logging capabilities, e.g. Firebug for Firefox
-        qx.log.appender.Native;
-        // support additional cross-browser console. Press F7 to toggle visibility
-        qx.log.appender.Console;
+
+        main: function () {
+          // Call super class
+          this.base(arguments);
+
+          // FIXME: enable only when dev flags
+          //if (qx.core.Environment.get("auth.mockBackend")) {
+          console.debug("Initializing FakeServer ...");
+          rest.dev.mock.Users;
+          //}
+
+          // Document is the application root
+          var doc = this.getRoot();
+
+          // Resources
+          let user = new qx.io.rest.Resource();
+          user.map('get', 'GET', 'api/v1/user/{id}', { id: qx.io.rest.Resource.REQUIRED }); // get user
+          // TODO: add check: { id: /\d+/ } }
+
+          let users = new qx.io.rest.Resource();
+          users.map('get', 'GET', 'api/v1/users/');  //Retrieve list of users 
+          users.map('post', 'POST', 'api/v1/users/'); //create user
+
+          users.addListener("success", function(e) {
+            console.debug(e.getId() + " event went well")
+          });
+
+          let store = new qx.data.store.Rest(users, "get");
+
+          var embed = new qx.ui.embed.Html();
+          embed.setBackgroundColor("white");
+          embed.setDecorator("main");
+          embed.setWidth(500);
+          embed.setHeight(200);
+          embed.setOverflow("auto", "auto");
+
+          doc.add(embed, { left: 10, top: 110 });
+
+          // after the data has been loaded
+          store.addListener("loaded", function () {
+            var model = store.getModel();
+            // display the model in the log
+            this.debug(qx.dev.Debug.debugProperties(model));
+            // display the model in an html embed
+            embed.setHtml(qx.dev.Debug.debugProperties(model, 10, true));
+          }, this);
+
+          users.longPoll("get");
+
+
+          // Create a button
+          var button1 = new qx.ui.form.Button("Click me", "rest/test.png");
+
+          // Add button to document at fixed coordinates
+          doc.add(button1, { left: 100, top: 50 });
+
+          // Add an event listener
+          button1.addListener("execute", function () {
+            /* eslint no-alert: "off" */
+            alert("Hello World!");
+          });
+        }
       }
-
-      /*
-      -------------------------------------------------------------------------
-        Below is your actual application code...
-      -------------------------------------------------------------------------
-      */
-
-      // Create a button
-      var button1 = new qx.ui.form.Button("Click me", "rest/test.png");
-
-      // Document is the application root
-      var doc = this.getRoot();
-
-      // Add button to document at fixed coordinates
-      doc.add(button1, {left: 100, top: 50});
-
-      // Add an event listener
-      button1.addListener("execute", function() {
-        /* eslint no-alert: "off" */
-        alert("Hello World!");
-      });
-    }
-  }
-});
+  });
