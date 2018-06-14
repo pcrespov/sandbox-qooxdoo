d86462f1a2206124db32cf149407984ca08baa78
Pedro Crespo <crespo@itis.ethz.ch>
2018-06-14 11:58:50 +0200

    Improved build script for dockerfile/qx-devel image variants

--- dockerfiles/qx-devel/scripts/build.sh
+++ dockerfiles/qx-devel/scripts/build.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-echo @a
+echo Running \'docker-compose build "$@"\'
 
 # produces qx:master
-docker-compose build
-docker-compose run qx /bin/bash -c "which qx && qx --help"
+docker-compose build "$@"
+docker-compose run qx /bin/bash -c "which qx && qx --help 2>&1 >/dev/null | grep  Versions"
+docker-compose down
 
 # produces qx:released
-BUILD_TARGET=released docker-compose build
-BUILD_TARGET=released docker-compose run qx /bin/bash -c "which qx && qx --help"
+BUILD_TARGET=released docker-compose build "$@"
+BUILD_TARGET=released docker-compose run qx /bin/bash -c "which qx && qx --help 2>&1 >/dev/null | grep  Versions"
+docker-compose down
 
 # display images
 docker image ls | grep qx
