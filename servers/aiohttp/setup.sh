62bc57eea2e213621fa9f6db571f6003b9d75c82
Pedro Crespo <crespo@itis.ethz.ch>
2018-06-20 07:45:36 +0200

    Created script to build a separate venv for every pytho module

--- servers/aiohttp/setup.sh
+++ servers/aiohttp/setup.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
-# FIXME:
-ENV_DIR=./.VENV 
+# https://packaging.python.org/tutorials/installing-packages
+
+CURRENT_DIRPATH=`dirname $(realpath $0)`
+DIRNAME=${CURRENT_DIRPATH##*/}
+ENV_DIR=~/.virtualenvs/$DIRNAME
+
+echo "Building virtual env at $ENV_DIR"
 
 python3 -m venv $ENV_DIR
 
 source $ENV_DIR/bin/activate
 $ENV_DIR/bin/pip install --upgrade pip
-$ENV_DIR/bin/pip install -r source/requirements.txt
+$ENV_DIR/bin/pip install -r requirements.txt
 
