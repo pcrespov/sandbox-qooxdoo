b9d83c8a7e69f09a0e53fa753ef96962ffaf970b
Pedro Crespo <crespo@itis.ethz.ch>
2018-06-22 17:42:33 +0200

    Script to create venv at the repo basedir

--- create-venv.sh
+++ create-venv.sh
@@ -4,13 +4,13 @@
 
 CURRENT_DIRPATH=`dirname $(realpath $0)`
 DIRNAME=${CURRENT_DIRPATH##*/}
-ENV_DIR=~/.virtualenvs/$DIRNAME
+#ENV_DIR=~/.virtualenvs/$DIRNAME
+ENV_DIR=.venv/qooxdoo-samples
 
-echo "Building virtual env at $ENV_DIR"
+echo "Building virtual env at $ENV_DIR ..." 
 
 python3 -m venv $ENV_DIR
 
-source $ENV_DIR/bin/activate
+#source $ENV_DIR/bin/activate
 $ENV_DIR/bin/pip install --upgrade pip
-$ENV_DIR/bin/pip install -r requirements.txt
-
+#$ENV_DIR/bin/pip install -r requirements-dev.txt
