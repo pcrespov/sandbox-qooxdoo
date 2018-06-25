ee7449527ad82534f0ecee22bf116203cb68b6ac
Pedro Crespo <crespo@itis.ethz.ch>
2018-06-25 21:20:59 +0200

    Deploy in docker: dockerize server and created compose

--- servers/aiohttp-api/src/server/utils/init_db.py
+++ servers/aiohttp-api/src/server/utils/init_db.py
@@ -1,21 +1,34 @@
 """ Initializes tables in database and adds some sample data for testing
 
+FIXME: this place does not fill right... see how this script is called
+
+
 References:
 [1]:https://github.com/aio-libs/aiohttp-demos/blob/master/docs/preparations.rst#environment
 """
-from sqlalchemy import create_engine, MetaData
+import logging
 
 from passlib.hash import sha256_crypt
+from sqlalchemy import (MetaData,
+                        create_engine)
+from tenacity import (retry,
+                      stop_after_attempt,
+                      wait_fixed)
 
-from server.model import users, permissions
-from server.config import SRC_DIR, get_config
+from server.config import (SRC_DIR,
+                           get_config)
+from server.model import (permissions,
+                          users)
+
+logging.basicConfig(level=logging.DEBUG)
+_LOGGER = logging.getLogger(__name__)
 
 
 DSN = "postgresql://{user}:{password}@{host}:{port}/{database}"
 
 ADMIN_DB_URL = DSN.format(
     user='postgres', password='postgres', database='postgres',
-    host='localhost', port=5432
+    host='db', port=5432
 )
 
 admin_engine = create_engine(ADMIN_DB_URL, isolation_level='AUTOCOMMIT')
@@ -75,26 +88,52 @@ def drop_tables(engine=test_engine):
 def sample_data(engine=test_engine):
 
     generate_password_hash = sha256_crypt.hash
-    
+
     conn = engine.connect()
     conn.execute(users.insert(), [
-        {'login': 'bizzy@itis.ethz.ch', 'passwd': generate_password_hash('z43'), 'is_superuser': False, 'disabled': False},
-        {'login': 'pcrespov', 'passwd': generate_password_hash('123'), 'is_superuser':True, 'disabled': False},
-        {'login': 'mrspam', 'passwd': generate_password_hash('345'), 'is_superuser': True, 'disabled': True}
-        ])
+        {'login': 'bizzy@itis.ethz.ch',
+         'passwd': generate_password_hash('z43'),
+         'is_superuser': False,
+         'disabled': False},
+        {'login': 'pcrespov',
+         'passwd': generate_password_hash('123'),
+         'is_superuser': True,
+         'disabled': False},
+        {'login': 'mrspam',
+         'passwd': generate_password_hash('345'),
+         'is_superuser': True,
+         'disabled': True}
+    ])
 
     conn.execute(permissions.insert(), [
-        {'user_id': 1, 'perm_name': 'tester'},
-        {'user_id': 2, 'perm_name': 'admin'}
+        {'user_id': 1,
+         'perm_name': 'tester'},
+        {'user_id': 2,
+         'perm_name': 'admin'}
     ])
 
     conn.close()
 
 
-if __name__ == '__main__':
+@retry(stop=stop_after_attempt(5), wait=wait_fixed(2))
+def main():
 
-    setup_db(USER_CONFIG['postgres'])
-    create_tables(engine=user_engine)
-    sample_data(engine=user_engine)
+    config = USER_CONFIG['postgres']
+    engine = user_engine
+
+    _LOGGER.info("Setting up db ...")
+    setup_db(config)
+    _LOGGER.info("")
+
+    _LOGGER.info("Creating tables ...")
+    create_tables(engine=engine)
+
+    _LOGGER.info("Adding sample data ...")
+    sample_data(engine=engine)
+
     # drop_tables()
     # teardown_db(config)
+
+
+if __name__ == '__main__':
+    main()
