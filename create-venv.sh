9ad8651bf5ac0de83cc3338e64a72de01d23b06f
Pedro Crespo <crespo@itis.ethz.ch>
2018-06-25 15:17:33 +0200

    Script installs all requirements for developmet as well

--- create-venv.sh
+++ create-venv.sh
@@ -1,21 +1,23 @@
-#!/bin/sh
-
-# https://packaging.python.org/tutorials/installing-packages
-
-#CURRENT_DIRPATH=`dirname $(realpath $0)`
-#DIRNAME=${CURRENT_DIRPATH##*/}
-#ENV_DIR=~/.virtualenvs/$DIRNAME
-
+#!/bin/bash
 # FIXME: every sub-folder under servers can have its own VENV
-# - venv at workspace level so vscode can find them
-ENV_DIR=.venv/aiohttp-api
+# TODO: how to activate current venv from batch??
+
+# NOTES: https://packaging.python.org/tutorials/installing-packages
+CURRENT_DIRPATH=`dirname $(realpath $0)`
+DIRNAME=${CURRENT_DIRPATH##*/}
+
+
+# venv at workspace level so vscode can find them
+ENV_DIR=${CURRENT_DIRPATH}/.venv/aiohttp-api
 
 echo "Building virtual env at $ENV_DIR ..." 
 
 python3 -m venv $ENV_DIR
 
-#source $ENV_DIR/bin/activate
+
+# TODO: create cli with this optional
+#source $ENV_DIR/bin/activate # source does not work in batch!
 $ENV_DIR/bin/pip install --upgrade pip
-pushd servers/aiohttp-api
+pushd servers/aiohttp-api/
 $ENV_DIR/bin/pip install -r requirements-dev.txt
 popd
